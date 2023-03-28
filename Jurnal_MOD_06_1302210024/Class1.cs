890707ebe9ed459977f247285c9bb4ee9d3b80eb
HelloWorldYessir420 <119160448+HelloWorldYessir420@users.noreply.github.com>
2023-03-28 16:54:18 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ Jurnal_MOD_06_1302210024/Class1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD_06_1302210024
+{
+    internal class SayaTubeVideo
+    {
+        private int id;
+        public string title;
+        private int playCount;
+
+        public SayaTubeVideo(String title)
+        {
+            if (title == null || title == "")
+            {
+                throw new ArgumentNullException("Judul Video Tidak Boleh Kosong");
+            }
+
+            if (title.Length > 200)
+            {
+                throw new ArgumentNullException("Judul Video Tidak Boleh Lebih Dari 200 Karakter");
+            }
+
+            this.id = new Random().Next(10000);
+            this.title = title;
+            this.playCount = 0;
+        }
+        public void IncreasePlayCount(int playCount)
+        {
+            if (playCount < 0 || playCount > 25000000)
+            {
+                throw new ArgumentOutOfRangeException("Input Tidak Boleh Kosong/Lebih Dari 25.000.000");
+            }
+
+            try
+            {
+                checked
+                {
+                    this.playCount += playCount;
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine("Error " + e.Message);
+            }
+        }
+
+        public int GetPLayCount()
+        {
+            return this.playCount;
+        }
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Film Berjudul : " + title);
+            Console.WriteLine("ID Film : " + id);
+            Console.WriteLine("Diputar Sebanyak : " + playCount + " Kali");
+            Console.WriteLine(" ");
+        }
+
+    }
+}
---
+++ Jurnal_MOD_06_1302210024/Class2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD_06_1302210024
+{
+    internal class SayaTubeUser
+    {
+        private int ID;
+        private List<SayaTubeVideo> uploadedVideos;
+        public string Username;
+
+        public SayaTubeUser(string Username)
+        {
+            if (Username == null || Username == "")
+            {
+                throw new ArgumentNullException("Username Harus Diisi");
+            }
+
+            if (Username.Length > 100)
+            {
+                throw new ArgumentNullException("Username Tidak Boleh Lebih Dari 200 Karakter");
+            }
+            
+            this.ID = 0;
+            this.Username = Username;
+            this.uploadedVideos = new List<SayaTubeVideo>();
+        }
+
+        public int GetTotalVideoPlayCount()
+        {
+            int totalVideoPlayCount = 0;
+            foreach(SayaTubeVideo video in uploadedVideos)
+            {
+                totalVideoPlayCount+= video.GetPLayCount();
+               
+            }
+            return totalVideoPlayCount;
+            
+        }
+
+        public void AddVideo(SayaTubeVideo video)
+        {
+            this.uploadedVideos.Add(video);
+        }
+
+        public void PrintAllVideoPlayCount()
+        {
+            for (int i = 0;i < uploadedVideos.Count;i++)
+            {
+                Console.WriteLine("Review Film " + (i+1) + " Judul : '" + uploadedVideos[i].title + 
+                    "' Play Count : " + uploadedVideos[i].GetPLayCount());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Oleh User : " + Username);
+        }
+
+    }
+}
